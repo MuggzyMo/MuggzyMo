d06ee79c7676e430de9167ffe585242711783745
Morgan <morgan.mcguire2000@outlook.com>
2024-10-05 02:39:11 -0400

    Resume download added

--- src/pages/home/IconLink.tsx
+++ src/pages/home/IconLink.tsx
@@ -1,19 +1,27 @@
 import { linkedIn, github, email } from "./Data";
+import resume from "../../assets/files/Morgan_McGuire_Resume.pdf";
 
 function IconLink() {
   return (
-    <div className="d-flex justify-content-evenly mt-2">
-      <h2>
-        <a href="https://www.linkedin.com/in/morgan-mcguire-1b7b9222b">
-          {linkedIn}
+    <div>
+      <div className="d-flex justify-content-evenly mt-2">
+        <h2>
+          <a href="https://www.linkedin.com/in/morgan-mcguire-1b7b9222b">
+            {linkedIn}
+          </a>
+        </h2>
+        <h2>
+          <a href="https://github.com/MuggzyMo">{github}</a>
+        </h2>
+        <h2>
+          <a href="mailto:morgan.mcguire2000@outlook.com">{email}</a>
+        </h2>
+      </div>
+      <div className="fs-4">
+        <a href={resume} download="Morgan_McGuire_Resume.pdf">
+          Download my resume!
         </a>
-      </h2>
-      <h2>
-        <a href="https://github.com/MuggzyMo">{github}</a>
-      </h2>
-      <h2>
-        <a href="mailto:morgan.mcguire2000@outlook.com">{email}</a>
-      </h2>
+      </div>
     </div>
   );
 }
